540b114822bc956ffd20a4b3aa17105627ec8eee
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2018-02-15 10:33:49 +0100

    added infra to list of insecure repos.

--- docker-machine-create-virtualbox.sh
+++ docker-machine-create-virtualbox.sh
@@ -4,5 +4,6 @@ docker-machine create \
     --driver virtualbox \
     --virtualbox-cpu-count 2 \
     --virtualbox-memory 2048 \
+    --engine-insecure-registry $(docker-machine ip infra):5000 \
     $@
 
