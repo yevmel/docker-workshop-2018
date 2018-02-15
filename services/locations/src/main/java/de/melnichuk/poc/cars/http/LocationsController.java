217ed6025df14a98916994a972eb21f1483829a0
Marco Paga <marco.paga@codecentric.de>
2018-02-15 10:47:31 +0100

    Logging added

--- services/locations/src/main/java/de/melnichuk/poc/cars/http/LocationsController.java
+++ services/locations/src/main/java/de/melnichuk/poc/cars/http/LocationsController.java
@@ -2,6 +2,8 @@ package de.melnichuk.poc.cars.http;
 
 import de.melnichuk.poc.cars.model.Car;
 import de.melnichuk.poc.cars.model.Location;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -19,16 +21,20 @@ import java.util.List;
 @RestController
 public class LocationsController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationsController.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/locations")
     public Collection<Location> locations() {
+        LOGGER.info("All locations requested");
         return getTestLocations();
     }
 
     @GetMapping("/locations/{name}")
     public Collection<Car> location(@PathVariable("name") String name) {
+        LOGGER.info("Locations for {} requested.", name);
         final URI uri = UriComponentsBuilder.fromHttpUrl("http://cars/").pathSegment("cars")
                 .queryParam("location", name)
                 .build().toUri();
